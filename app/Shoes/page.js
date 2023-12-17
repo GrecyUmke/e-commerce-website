c6c3b6d377177f05911e584788a5779584966565
maggie <ranirathor20@navgurukul.org>
2023-12-17 11:19:32 +0530

    Working on Product Listing Page

--- app/Shoes/page.js
+++ app/Shoes/page.js
@@ -1,31 +1,132 @@
+
 const Shoes =()=>{
     return(
         <div class="container mt-5">
+
         <div class="row">
-            <div class="col-md-4 offset-md-30">
-                <div class="card">
-                    <img src="https://rukminim2.flixcart.com/image/300/350/xif0q/shoe/7/2/m/6-tm-12-6-trm-white-original-imagjqyzz8z9jrgf.jpeg?q=90" class="card-img-top" alt="Your Image"></img>
-                    <div class="card-body">
+            <div class="col-lg-4 col-md-6 col-sm-10">
+                <div class="card product-card">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <img src="https://rukminim2.flixcart.com/image/832/832/xif0q/shoe/a/a/p/9-axe-white-9-hotstyle-white-original-imagthgrt9ae7g4x.jpeg?q=70" class="card-img-top product-image" alt="Product Image 1" style={{width:"414px",height:"500px"}}></img>
+                        </div>
+                        
                     </div>
                 </div>
             </div>
-            <div class="col-md-7">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Shoes</h5>
-                        <p class="card-text">Premium Stylish Comfortable Shoe Running Shoes For Men (Navy)</p>
-                        <p>Bank Offer10% Instant Discount on PNB Credit Cards, up to ₹1500, on orders of ₹5,000 and aboveT&C</p>
-                        <p>Bank Offer10% off on Bank of Baroda Credit Card EMI Txns, up to ₹2,000 on orders of ₹5,000 and aboveT&C</p>
-                        <p>Special PriceGet extra 31% off (price inclusive of cashback/coupon)T&C</p>
-                        <p>460</p>
-                    </div>
+            <div class="col-md-6">
+                <div class="card-body">
+                    <h5 class="card-title product-title">Product 1</h5>
+                        <p class="card-text product-description">Product 1 description.</p>
+                        <p class="card-text product-price">$20.99</p>
+                        <a href="#" class="btn btn-primary">Add to Cart</a>
                 </div>
             </div>
         </div>
-        <button type="button" class="btn btn-warning col-md-2 offset-md-30">ADD TO CART</button>
-        <button type="button" class="btn btn-danger col-md-2 offset-md-30">BUY NOW</button>
-    </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-6 col-sm-10">
+                <div class="card product-card">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <img src="https://img3.junaroad.com/uiproducts/19720587/pri_175_p-1681889786.jpg" class="card-img-top product-image" alt="Product Image 1" style={{width:"414px",height:"500px"}}></img>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card-body">
+                    <h5 class="card-title product-title">Product 1</h5>
+                        <p class="card-text product-description">Product 1 description.</p>
+                        <p class="card-text product-price">$20.99</p>
+                        <a href="#" class="btn btn-primary">Add to Cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-6 col-sm-10">
+                <div class="card product-card">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <img src="https://www.campusshoes.com/cdn/shop/products/RAISE_22L-129_ANGLFALS-MUNLT.jpg?v=1670325436" class="card-img-top product-image" alt="Product Image 1" style={{width:"414px",height:"500px"}}></img>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card-body">
+                    <h5 class="card-title product-title">Product 1</h5>
+                        <p class="card-text product-description">Product 1 description.</p>
+                        <p class="card-text product-price">$20.99</p>
+                        <a href="#" class="btn btn-primary">Add to Cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-6 col-sm-10">
+                <div class="card product-card">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <img src="https://rukminim2.flixcart.com/image/850/1000/xif0q/shoe/n/l/5/-original-imaggkz4dxarymrs.jpeg?q=90" class="card-img-top product-image" alt="Product Image 1" style={{width:"414px",height:"500px"}}></img>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card-body">
+                    <h5 class="card-title product-title">Product 1</h5>
+                        <p class="card-text product-description">Product 1 description.</p>
+                        <p class="card-text product-price">$20.99</p>
+                        <a href="#" class="btn btn-primary">Add to Cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-6 col-sm-10">
+                <div class="card product-card">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <img src="https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/18274390/2022/5/14/c64644f6-840f-4496-bb42-19359e9881771652523841987RoadsterMenBrownTexturedPUSneakers1.jpg" class="card-img-top product-image" alt="Product Image 1" style={{width:"414px",height:"500px"}}></img>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card-body">
+                    <h5 class="card-title product-title">Product 1</h5>
+                        <p class="card-text product-description">Product 1 description.</p>
+                        <p class="card-text product-price">$20.99</p>
+                        <a href="#" class="btn btn-primary">Add to Cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-6 col-sm-10">
+                <div class="card product-card">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <img src="https://rukminim2.flixcart.com/image/850/1000/xif0q/shoe/j/u/g/-original-imaghrbyhgp8xxgd.jpeg?q=20" class="card-img-top product-image" alt="Product Image 1" style={{width:"414px",height:"500px"}}></img>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card-body">
+                    <h5 class="card-title product-title">Product 1</h5>
+                        <p class="card-text product-description">Product 1 description.</p>
+                        <p class="card-text product-price">$20.99</p>
+                        <a href="#" class="btn btn-primary">Add to Cart</a>
+                </div>
+            </div>
+        </div>
+
         
+    </div>  
     )
 } 
 export default Shoes
+
